ac43a047b86298bb97aeff1648d41b79da83a580
darkseid <gh0stv3g3ta@gmail.com>
2025-08-25 16:10:52 +0530

    feat: added StreamId parsing logic

---
+++ src/frame/stream_id.rs
@@ -0,0 +1,73 @@
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub struct StreamId(u32);
+
+#[derive(Debug, Copy, Clone)]
+pub struct StreamIdOverflow;
+
+const STREAM_ID_MASK: u32 = 1 << 31;
+
+impl StreamId {
+    pub const ZERO: StreamId = StreamId(0);
+
+    pub const MAX: StreamId = StreamId(u32::MAX >> 1);
+
+    /// buf = Reserved (1 bit) + Stream ID (31 bits)
+    #[inline]
+    pub fn parse(buf: &[u8]) -> (StreamId, bool) {
+        let mut ubuf = [0; 4];
+        ubuf.copy_from_slice(&buf[0..4]);
+        let unpacked = u32::from_be_bytes(ubuf);
+        // flag = MSB
+        let flag = unpacked & STREAM_ID_MASK == STREAM_ID_MASK;
+
+        // Now clear the MSB, as that is reserved and MUST be ignored when
+        // received.
+        (StreamId(unpacked & !STREAM_ID_MASK), flag)
+    }
+
+    pub fn is_client_initiated(&self) -> bool {
+        self.0 != 0 && self.0 % 2 == 1
+    }
+
+    pub fn is_server_initiated(&self) -> bool {
+        self.0 != 0 && self.0 % 2 == 0
+    }
+
+    #[inline]
+    pub fn zero() -> StreamId {
+        StreamId::ZERO
+    }
+
+    pub fn is_zero(&self) -> bool {
+        self.0 == 0
+    }
+
+    // TODO: check if needed ?
+    pub fn next_id(&self) -> Result<StreamId, StreamIdOverflow> {
+        let next = self.0 + 2;
+        if next > StreamId::MAX.0 {
+            Err(StreamIdOverflow)
+        } else {
+            Ok(StreamId(next))
+        }
+    }
+}
+
+impl From<u32> for StreamId {
+    fn from(src: u32) -> Self {
+        assert_eq!(src & STREAM_ID_MASK, 0, "invalid stream ID -- MSB is set");
+        StreamId(src)
+    }
+}
+
+impl From<StreamId> for u32 {
+    fn from(src: StreamId) -> Self {
+        src.0
+    }
+}
+
+impl PartialEq<u32> for StreamId {
+    fn eq(&self, other: &u32) -> bool {
+        self.0 == *other
+    }
+}
