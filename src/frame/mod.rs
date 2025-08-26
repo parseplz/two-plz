ce02e5d87f62c21a22db365247ca12e7c47f14dc
darkseid <gh0stv3g3ta@gmail.com>
2025-08-26 16:17:21 +0530

    feat: added unpack_octets_4 macro

--- src/frame/mod.rs
+++ src/frame/mod.rs
@@ -1,6 +1,35 @@
-mod head;
-mod stream_id;
+/// A helper macro that unpacks a sequence of 4 bytes found in the buffer with
+/// the given identifier, starting at the given offset, into the given integer
+/// type. Obviously, the integer type should be able to support at least 4
+/// bytes.
+///
+/// # Examples
+///
+/// ```ignore
+/// # // We ignore this doctest because the macro is not exported.
+/// let buf: [u8; 4] = [0, 0, 0, 1];
+/// assert_eq!(1u32, unpack_octets_4!(buf, 0, u32));
+/// ```
+macro_rules! unpack_octets_4 {
+    // TODO: Get rid of this macro
+    ($buf:expr, $offset:expr, $tip:ty) => {
+        (($buf[$offset + 0] as $tip) << 24)
+            | (($buf[$offset + 1] as $tip) << 16)
+            | (($buf[$offset + 2] as $tip) << 8)
+            | (($buf[$offset + 3] as $tip) << 0)
+    };
+}
 
+mod data;
+mod error;
+mod head;
+mod priority;
+mod reason;
+mod reset;
+mod stream_id;
+mod util;
+
+pub use self::head::{Head, Kind};
 pub use stream_id::StreamId;
 
 pub const HEADER_LEN: usize = 9;
