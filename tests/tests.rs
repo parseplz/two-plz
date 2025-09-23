0a4fb235430af3a54bb160713b16a8e03baa7ff0
darkseid <gh0stv3g3ta@gmail.com>
2025-09-23 15:33:10 +0530

    test: added mock server

---
+++ tests/tests.rs
@@ -0,0 +1,15 @@
+use bytes::BytesMut;
+use tokio::{io::AsyncReadExt, net::TcpListener};
+
+#[tokio::test]
+async fn mock_server() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    let (mut client_tcp, _) = listener.accept().await?;
+
+    let mut buf = BytesMut::new();
+    let _ = client_tcp.read_buf(&mut buf).await?;
+    let _ = client_tcp.read_buf(&mut buf).await?;
+
+    dbg!(buf);
+    Ok(())
+}
