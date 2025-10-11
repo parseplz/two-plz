4a2def27fe81d9c10be7f2cc4af207fbf9ea95fb
darkseid <gh0stv3g3ta@gmail.com>
2025-10-11 07:44:02 +0530

    feat: implemented Peer helper methods

--- src/proto/config.rs
+++ src/proto/config.rs
@@ -1,6 +1,60 @@
 use std::time::Duration;
 
+use tracing::error;
+
 use crate::frame::{Settings, StreamId};
+
+#[derive(Clone, Debug)]
+pub enum PeerRole {
+    Server,
+    Client {
+        /// Initial maximum number of locally initiated (send) streams.
+        /// After receiving a SETTINGS frame from the remote peer,
+        /// the connection will overwrite this value with the
+        /// MAX_CONCURRENT_STREAMS specified in the frame.
+        /// If no value is advertised by the remote peer in the initial SETTINGS
+        /// frame, it will be set to usize::MAX.
+        initial_max_send_streams: usize,
+        stream_id: StreamId,
+    },
+}
+
+impl PeerRole {
+    fn server() -> PeerRole {
+        PeerRole::Server
+    }
+
+    fn client() -> PeerRole {
+        PeerRole::Client {
+            initial_max_send_streams: usize::MAX,
+            stream_id: StreamId::ZERO,
+        }
+    }
+
+    fn set_max_send_streams(&mut self, max_send_streams: usize) {
+        if let PeerRole::Client {
+            initial_max_send_streams,
+            ..
+        } = self
+        {
+            *initial_max_send_streams = max_send_streams;
+        } else {
+            error!("set_max_send_streams called on server")
+        }
+    }
+
+    fn set_initial_stream_id(&mut self, id: StreamId) {
+        if let PeerRole::Client {
+            stream_id,
+            ..
+        } = self
+        {
+            *stream_id = id;
+        } else {
+            error!("set_initial_stream_id called on server")
+        }
+    }
+}
 
 #[derive(Clone, Debug)]
 pub struct ConnectionConfig {
@@ -21,13 +75,4 @@ pub struct ConnectionConfig {
     /// TODO: may be store in send and recv settings ?
     pub settings: Settings,
     pub peer_settings: Settings,
-}
-
-#[derive(Clone, Debug)]
-pub enum PeerRole {
-    Server,
-    Client {
-        initial_max_send_streams: usize,
-        stream_id: StreamId,
-    },
 }
