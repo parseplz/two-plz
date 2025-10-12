f97e5cd301b358c8c60bcb2781d7a0875255a812
darkseid <gh0stv3g3ta@gmail.com>
2025-10-12 08:55:00 +0530

    tests: removed old util functions

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,22 +1,3 @@
 #![allow(warnings, dead_code)]
-
-use tokio::io::{Empty, empty};
-use two_plz::{
-    Codec, Connection,
-    preface::Role,
-    proto::{
-        config::{ConnectionConfig, PeerRole},
-        connection::{ServerToUser, UserToServer},
-    },
-};
 mod mock_server;
-mod ping_pong;
-extern crate two_plz;
-
-fn build_server() -> Connection<Empty, ServerToUser, UserToServer> {
-    Connection::server(ConnectionConfig::default(), Codec::new(empty()))
-}
-
-fn build_client() -> Connection<Empty, UserToServer, ServerToUser> {
-    Connection::client(ConnectionConfig::default(), Codec::new(empty()))
-}
+mod test_ping_pong;
