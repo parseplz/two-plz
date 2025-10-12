01809379c4fe2ab7769c0ae4d032d0feb5977e0b
darkseid <gh0stv3g3ta@gmail.com>
2025-10-12 16:06:36 +0530

    tests: modified ping tests to use macros

--- tests/test_ping_pong.rs
+++ tests/test_ping_pong.rs
@@ -1,3 +1,4 @@
+use futures::StreamExt;
 use two_plz::{
     frame::{Frame, Ping},
     state::read::{ReadState, read_runner},
@@ -6,18 +7,31 @@ mod common;
 use common::*;
 
 #[test]
-fn test_ping_pong() {
+fn recv_single_ping() {
     let mut conn = build_server();
     let frame = Ping::new(Default::default());
     let state = read_runner(&mut conn, frame.into()).unwrap();
     assert_eq!(state, ReadState::NeedsFlush);
     write_to_read_buf(&mut conn);
     let frame = conn.read_frame().unwrap();
-    if let Frame::Ping(ping) = frame {
-        assert_eq!(ping, Ping::pong(Default::default()));
-    } else {
-        panic!()
-    }
-
-    //
+    let ping = assert_ping!(frame);
+    assert_eq!(ping, Ping::pong(Default::default()));
 }
+
+#[tokio::test]
+async fn recv_multiple_ping() {
+    let mut conn = build_server();
+    for i in 1..3 {
+        let frame = Ping::new([i; 8]);
+        let state = read_runner(&mut conn, frame.into()).unwrap();
+        assert_eq!(state, ReadState::NeedsFlush);
+    }
+    write_to_read_buf(&mut conn);
+    for i in 1..3 {
+        let ping = poll_frame!(Ping, conn.stream);
+        assert_eq!(ping, Ping::pong([i; 8]));
+    }
+}
+
+// TODO
+//async fn user_notifies_when_connection_closes() {
