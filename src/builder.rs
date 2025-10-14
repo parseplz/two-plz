e277caa7f9cf63a66ae1ce46e95d1014760a8f5e
darkseid <gh0stv3g3ta@gmail.com>
2025-10-14 17:43:19 +0530

    feat: implented helper methods for role

--- src/builder.rs
+++ src/builder.rs
@@ -1,9 +1,28 @@
 use crate::{Settings, StreamId, proto};
 use std::time::Duration;
 
+#[derive(PartialEq, Clone)]
 pub enum Role {
     Client,
     Server,
+}
+
+impl Role {
+    pub fn is_server(&self) -> bool {
+        matches!(self, Self::Server)
+    }
+
+    pub fn is_client(&self) -> bool {
+        matches!(self, Self::Client)
+    }
+
+    pub fn init_stream_id(&self) -> StreamId {
+        if self.is_server() {
+            2.into()
+        } else {
+            1.into()
+        }
+    }
 }
 
 pub struct Builder {
