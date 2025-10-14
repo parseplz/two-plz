1a10af4b578ec6b4a74f0da3b33db021ff2fe47c
darkseid <gh0stv3g3ta@gmail.com>
2025-10-14 18:01:34 +0530

    feat: implemented Recv struct

--- src/proto/recv.rs
+++ src/proto/recv.rs
@@ -1,27 +1,22 @@
 use std::time::Duration;
 
 use crate::{
-    StreamId,
+    DEFAULT_INITIAL_WINDOW_SIZE, StreamId,
+    builder::Role,
     proto::{
-        WindowSize, buffer::Buffer, flow_control::FlowControl, store::Queue,
-        stream::NextResetExpire,
+        WindowSize, buffer::Buffer, config::ConnectionConfig,
+        flow_control::FlowControl, store::Queue, stream::NextResetExpire,
     },
     stream_id::StreamIdOverflow,
 };
 
 #[derive(Debug)]
 pub(super) struct Recv {
-    /// Initial window size of remote initiated streams
-    init_window_sz: WindowSize,
+    /// Holds frames that are waiting to be read
+    buffer: Buffer<Event>,
 
     /// Connection level flow control governing received data
     flow: FlowControl,
-
-    /// Amount of connection window capacity currently used by outstanding streams.
-    in_flight_data: WindowSize,
-
-    /// The lowest stream ID that is still idle
-    next_stream_id: Result<StreamId, StreamIdOverflow>,
 
     /// The stream ID of the last processed stream
     last_processed_id: StreamId,
@@ -35,22 +30,23 @@ pub(super) struct Recv {
     /// > the identified last stream.
     max_stream_id: StreamId,
 
-    ///// Streams that have pending window updates
-    //pending_window_updates: Queue<NextWindowUpdate>,
+    /// The lowest stream ID that is still idle
+    next_stream_id: Result<StreamId, StreamIdOverflow>,
+
+    /// Streams that have pending window updates
+    /// pending_window_updates: Queue<NextWindowUpdate>,
     /// Locally reset streams that should be reaped when they expire
     pending_reset_expired: Queue<NextResetExpire>,
 
     /// How long locally reset streams should ignore received frames
     reset_duration: Duration,
-
-    /// Holds frames that are waiting to be read
-    buffer: Buffer<Event>,
-
-    /// If push promises are allowed to be received.
-    is_push_enabled: bool,
-
-    /// If extended connect protocol is enabled.
-    is_extended_connect_protocol_enabled: bool,
+    //
+    // TODO
+    ///// If push promises are allowed to be received.
+    //is_push_enabled: bool,
+    //
+    ///// If extended connect protocol is enabled.
+    //is_extended_connect_protocol_enabled: bool,
 }
 
 #[derive(Debug)]
