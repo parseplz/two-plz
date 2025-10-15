31f724a7f405b340b774caf223e8d882e75c47b3
darkseid <gh0stv3g3ta@gmail.com>
2025-10-15 10:21:02 +0530

    feat: implemented Counts::new

--- src/proto/count.rs
+++ src/proto/count.rs
@@ -1,15 +1,17 @@
+use crate::proto::config::ConnectionConfig;
+
 #[derive(Debug)]
 pub(super) struct Counts {
     /// Maximum number of locally initiated streams
     max_send_streams: usize,
 
-    /// Current number of remote initiated streams
+    /// Current number of locally initiated streams
     num_send_streams: usize,
 
     /// Maximum number of remote initiated streams
     max_recv_streams: usize,
 
-    /// Current number of locally initiated streams
+    /// Current number of remote initiated streams
     num_recv_streams: usize,
 
     /// Maximum number of pending locally reset streams
@@ -27,4 +29,29 @@ pub(super) struct Counts {
     /// Total number of locally reset streams due to protocol error across the
     /// lifetime of the connection.
     num_local_error_reset_streams: usize,
+}
+
+impl Counts {
+    /// Create a new `Counts` using the provided configuration values.
+    pub fn new(config: &ConnectionConfig) -> Self {
+        Counts {
+            max_send_streams: config
+                .local_settings
+                .max_concurrent_streams()
+                .map(|v| v as usize)
+                .unwrap_or(usize::MAX),
+            num_send_streams: 0,
+            max_recv_streams: config
+                .peer_settings
+                .max_concurrent_streams()
+                .map(|v| v as usize)
+                .unwrap_or(usize::MAX),
+            num_recv_streams: 0,
+            max_local_reset_streams: config.reset_stream_max,
+            num_local_reset_streams: 0,
+            max_local_error_reset_streams: config
+                .local_max_error_reset_streams,
+            num_local_error_reset_streams: 0,
+        }
+    }
 }
