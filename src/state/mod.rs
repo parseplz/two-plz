3258127abd3083cddb7c3e1710da7827c56c523c
darkseid <gh0stv3g3ta@gmail.com>
2025-10-29 18:39:32 +0530

    feat: removed connection_runner

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -4,27 +4,3 @@ use tokio::io::{AsyncRead, AsyncWrite};
 
 pub mod read;
 mod write;
-
-// Run the entire client/server connection loop
-// E => Sent to user
-// U => Received from user
-async fn connection_runner<T, E, U>(mut conn: Connection<T, E, U>)
-where
-    T: AsyncRead + AsyncWrite + Unpin,
-{
-    tokio::select! {
-        frame = conn.codec.next() => {
-            match frame {
-                Some(Ok(frame)) => {
-                    let state_result = read_runner(&mut conn, frame);
-                    todo!()
-                }
-                Some(Err(_)) => todo!(),
-                None => todo!(),
-            }
-        }
-        msg = conn.handler.recv() => {
-            todo!()
-        }
-    }
-}
