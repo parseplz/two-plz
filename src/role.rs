2ecd1326ac96a40c6c4a2b1664a3976c995568eb
darkseid <gh0stv3g3ta@gmail.com>
2025-10-30 08:43:35 +0530

    feat: ported Role::can_open method

--- src/role.rs
+++ src/role.rs
@@ -1,4 +1,7 @@
-use crate::StreamId;
+use crate::{
+    Reason, StreamId,
+    proto::{ProtoError, recv::Open},
+};
 
 #[derive(PartialEq, Clone)]
 pub enum Role {
@@ -26,5 +29,34 @@ impl Role {
     pub fn is_local_init(&self, id: StreamId) -> bool {
         assert!(!id.is_zero());
         self.is_server() == id.is_server_initiated()
+    }
+
+    /// Returns true if the remote peer can initiate a stream with the given ID.
+    pub fn ensure_can_open(
+        &self,
+        id: StreamId,
+        mode: Open,
+    ) -> Result<(), ProtoError> {
+        if self.is_server() {
+            // Ensure that the ID is a valid client initiated ID
+            if mode.is_push_promise() || !id.is_client_initiated() {
+                proto_err!(conn: "cannot open stream {:?} - not client initiated", id);
+                return Err(ProtoError::library_go_away(
+                    Reason::PROTOCOL_ERROR,
+                ));
+            }
+
+            Ok(())
+        } else {
+            // Ensure that the ID is a valid server initiated ID
+            if !mode.is_push_promise() || !id.is_server_initiated() {
+                proto_err!(conn: "cannot open stream {:?} - not server initiated", id);
+                return Err(ProtoError::library_go_away(
+                    Reason::PROTOCOL_ERROR,
+                ));
+            }
+
+            Ok(())
+        }
     }
 }
