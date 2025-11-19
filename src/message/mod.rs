541c00ff75ef298d20458001f358f7c3bd06acd6
darkseid <gh0stv3g3ta@gmail.com>
2025-11-19 16:35:27 +0530

    feat: implemente TwoTwo methods
    1. take_body
    2. take_trailer
    3. into_message_head
    4. into_header_frame

--- src/message/mod.rs
+++ src/message/mod.rs
@@ -1,8 +1,9 @@
 use bytes::BytesMut;
 use http::{HeaderMap, HeaderValue};
+
+use crate::{StreamId, frame, headers::Pseudo};
 pub mod request;
 pub mod response;
-
 
 #[derive(Debug)]
 pub struct TwoTwo<T> {
@@ -11,3 +12,34 @@ pub struct TwoTwo<T> {
     body: Option<BytesMut>,
     trailer: Option<HeaderMap<HeaderValue>>,
 }
+
+impl<T> TwoTwo<T>
+where
+    T: InfoLine,
+{
+    fn take_body(&mut self) -> Option<BytesMut> {
+        self.body.take()
+    }
+
+    fn take_trailer(&mut self) -> Option<HeaderMap<HeaderValue>> {
+        self.trailer.take()
+    }
+
+    pub fn into_message_head(self) -> (T, HeaderMap<HeaderValue>) {
+        (self.info_line, self.headers)
+    }
+
+    pub fn into_header_frame(
+        self,
+        stream_id: StreamId,
+        end_of_stream: bool,
+    ) -> frame::Headers {
+        let pseudo = self.info_line.into_pseudo();
+        let mut frame = frame::Headers::new(stream_id, pseudo, self.headers);
+        if end_of_stream {
+            frame.set_end_stream()
+        }
+        frame
+    }
+}
+
