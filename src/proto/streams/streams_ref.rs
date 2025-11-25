150b28cbfae399e0a98d3011cee2c978a90ca1ab
darkseid <gh0stv3g3ta@gmail.com>
2025-11-25 18:23:28 +0530

    feat: implemented send_response method for StreamRef

--- src/proto/streams/streams_ref.rs
+++ src/proto/streams/streams_ref.rs
@@ -39,4 +39,53 @@ impl<B> StreamRef<B> {
             .recv
             .take_request(&mut stream)
     }
+
+    pub fn send_response(
+        &mut self,
+        mut response: Response,
+    ) -> Result<(), UserError> {
+        let mut me = self.opaque.inner.lock().unwrap();
+        let me = &mut *me;
+        let mut stream = me.store.resolve(self.opaque.key);
+        let span =
+            tracing::debug_span!("[+] send response| ", "{:?}", stream.id);
+        let _ = span.enter();
+        let actions = &mut me.actions;
+        let mut send_buffer = self.send_buffer.inner.lock().unwrap();
+        let send_buffer = &mut *send_buffer;
+        let mut response_frames = TwoTwoFrame::from((stream.id, response));
+        let data_frame = response_frames.take_data();
+        let trailer_frame = response_frames.take_trailer();
+
+        me.counts
+            .transition(stream, |counts, stream| {
+                actions.send.send_headers(
+                    response_frames.header,
+                    send_buffer,
+                    stream,
+                    counts,
+                    &mut actions.task,
+                )
+            });
+        trace!("[+] added| header");
+        let mut stream = me.store.resolve(self.opaque.key);
+        if let Some(frame) = data_frame {
+            trace!("[+] added| data");
+            stream.remaining_data_len = Some(frame.payload().len());
+            stream
+                .pending_send
+                .push_back(send_buffer, frame.into());
+        }
+
+        if let Some(frame) = trailer_frame {
+            trace!("[+] added| trailer");
+            stream
+                .pending_send
+                .push_back(send_buffer, frame.into());
+        }
+        stream.state.send_close();
+
+        trace!("[+] response queued");
+        Ok(())
+    }
 }
