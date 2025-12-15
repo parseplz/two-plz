a411b00477cd07b57eac8136f1d04080848f9262
darkseid <gh0stv3g3ta@gmail.com>
2025-12-15 12:32:48 +0530

    test: filte traces from hpack and codec

--- tests/support/src/trace.rs
+++ tests/support/src/trace.rs
@@ -1,10 +1,18 @@
 pub use tracing;
+use tracing::Level;
+use tracing::level_filters::LevelFilter;
 pub use tracing_subscriber;
 
+use tracing_subscriber::filter;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 
 pub fn init() -> tracing::dispatcher::DefaultGuard {
+    let filter = filter::Targets::new()
+        .with_default(Level::TRACE) // Default level INFO
+        .with_target("two_plz::hpack", LevelFilter::OFF)
+        .with_target("two_plz::codec", LevelFilter::OFF);
+    //.with_target("two_plz::frame", LevelFilter::OFF);
     let use_colors = atty::is(atty::Stream::Stdout);
     let layer = tracing_tree::HierarchicalLayer::default()
         .with_writer(tracing_subscriber::fmt::writer::TestWriter::default())
@@ -15,5 +23,6 @@ pub fn init() -> tracing::dispatcher::DefaultGuard {
 
     tracing_subscriber::registry()
         .with(layer)
+        .with(filter)
         .set_default()
 }
